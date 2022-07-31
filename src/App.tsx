69a3e73d53022b33220346dae51eb48a87fb3bb7
arunmani <arunmani9787@gmail.com>
2022-07-31 18:32:52 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/navbar/NavBar";
+import MainSection from "./components/main/MainSection";
+import Progress from "./components/Progress/Progress";
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      {/* <MainSection/> */}
+      <Progress/>
     </div>
   );
 }
